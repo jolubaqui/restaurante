c7a342a36850bec7479cffdbaa710b487eb08590
jose barros <jolubaqui@gmail.com>
2024-03-13 16:38:50 -0500

    Comienza page principal

--- index.php
+++ index.php
@@ -1,3 +1,73 @@
-<?php
-echo "Hola Mundo";
-?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Restaurante</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+
+            <a class="navbar-brand" href="#">Bonka's</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+
+                <ul class="nav navbar-nav ml-auto">
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Menú día</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Chefs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Testimonios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Comentarios</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <section id="banner" class="container-fluid p-0">
+        <div class="banner-img" style="position:relative; background:url('images/foodRestaurant.jpg') center/cover no-repeat; height:400px;">
+            <div class="banner-text" style="position:absolute; top:50%; left: 57%; text-align:center; color:#fff;">
+                <h1>Bonka's</h1>
+                <p>Restaurante Gourmet, sin carne molida</p>
+                <a href="#menu" class="btn btn-primary">Ver Menú</a>
+            </div>
+        </div>
+    </section>
+    <br />
+    <header>
+        <!-- place navbar here -->
+    </header>
+    <main></main>
+    <footer class="bg-dark text-light text-center">
+        <!-- place footer here -->
+        <p>&copy; 2024 Bonka's, todos los derechos reservados.</p>
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+
+</html>
