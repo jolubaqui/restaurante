a008cd1f5a5c53bbe6b3dba580303adbcc9179c2
jose barros <jolubaqui@gmail.com>
2024-03-14 17:16:41 -0500

    creando comentarios y colaboradores

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
@@ -16,7 +17,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
 
-            <a class="navbar-brand" href="#">Bonka's</a>
+            <a class="navbar-brand" href="#"> <i class="fas fa-utensils"></i> Bonka's</a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -46,14 +47,116 @@
         </div>
     </nav>
     <section id="banner" class="container-fluid p-0">
-        <div class="banner-img" style="position:relative; background:url('images/foodRestaurant.jpg') center/cover no-repeat; height:400px;">
-            <div class="banner-text" style="position:absolute; top:50%; left: 57%; text-align:center; color:#fff;">
+        <div class="banner-img" style="position:relative; background:url('images/foodRestaurant3.jpg') center/cover no-repeat; height:500px;">
+            <div class="banner-text" style="position:absolute; top:50%; left: 50%; transform: translate(-50%, -50%); text-align:center; color:#fff">
                 <h1>Bonka's</h1>
-                <p>Restaurante Gourmet, sin carne molida</p>
+                <p style="color:black;">Gusto auténtico, sabores únicos!</p>
                 <a href="#menu" class="btn btn-primary">Ver Menú</a>
             </div>
         </div>
     </section>
+    <section id="id" class="container mt-4 text-center">
+
+        <div class="jumbotron bg-dark text-white">
+            <br>
+            <h2>Bienvenido al restaurante Bonka's</h2>
+            <p>Descubre una cocina con pasión, servida con amor. </p>
+            <br>
+        </div>
+
+    </section>
+
+    <section id="chefs" class="container mt-4 text-center">
+        <h2>Nuestros Chefs!</h2>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="images/colaboradores/Chef-Carlo.jpg" alt="Carlo" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Carlo</h5>
+                        <p class="card-text">Especialidades en comida francesa</p>
+                        <div class="social-icons mt-3">
+                            <a href="#" class="text-dark me-2"><i class="fab fa-facebook"></i></a>
+                            <a href="#" class="text-dark me-2"><i class="fab fa-instagram"></i></a>
+                            <a href="#" class="text-dark me-2"><i class="fab fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+
+                    <img src="images/colaboradores/Chef-Chan.jpg" alt="Chan" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Chan</h5>
+                        <p class="card-text">Especialidades en comida China</p>
+                        <div class="social-icons mt-3">
+                            <a href="#" class="text-dark me-2"><i class="fab fa-facebook"></i></a>
+                            <a href="#" class="text-dark me-2"><i class="fab fa-instagram"></i></a>
+                            <a href="#" class="text-dark me-2"><i class="fab fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="images/colaboradores/Chef-Nicola.jpg" alt="Nicola" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Nicola</h5>
+                        <p class="card-text">Especialidades en comida italiana</p>
+                        <div class="social-icons mt-3">
+                            <a href="#" class="text-dark me-2"><i class="fab fa-facebook"></i></a>
+                            <a href="#" class="text-dark me-2"><i class="fab fa-instagram"></i></a>
+                            <a href="#" class="text-dark me-2"><i class="fab fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section id="testimonios" class="bg-light py-5">
+        <div class="container">
+            <h2 class="text-center mb-4">Testimonios</h2>
+            <div class="row">
+                <div class="col-md-6 d-flex">
+                    <div class="card mb-4 w-100">
+                        <div class="card-body">
+                            <p class="card-text">Muy buena comida</p>
+                        </div>
+                        <div class="card-footer text-muted">
+                            Andres Guerra
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex">
+                    <div class="card mb-4 w-100">
+                        <div class="card-body">
+                            <p class="card-text">Muy buena comida</p>
+                        </div>
+                        <div class="card-footer text-muted">
+                            Andres Guerra
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex">
+                    <div class="card mb-4 w-100">
+                        <div class="card-body">
+                            <p class="card-text">Muy buena comida</p>
+                        </div>
+                        <div class="card-footer text-muted">
+                            Andres Guerra
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <br />
     <header>
         <!-- place navbar here -->
