49380d4fcd5ad8f9bd950258946e5895fc311c92
jose barros <jolubaqui@gmail.com>
2024-03-15 17:47:47 -0500

    Añadiendo contacto y menu

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav id="inicio" class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
 
             <a class="navbar-brand" href="#"> <i class="fas fa-utensils"></i> Bonka's</a>
@@ -22,30 +22,34 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
+            <!--barra de Navegación-->
             <div class="collapse navbar-collapse" id="navbarNav">
 
                 <ul class="nav navbar-nav ml-auto">
 
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Inicio</a>
+                        <a class="nav-link" href="#inicio">Inicio</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Menú día</a>
+                        <a class="nav-link" href="#menu">Menú día</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Chefs</a>
+                        <a class="nav-link" href="#chefs">Chefs</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Testimonios</a>
+                        <a class="nav-link" href="#testimonios">Testimonios</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Comentarios</a>
+                        <a class="nav-link" href="#contacto">Contacto</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#horario">Horarios</a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
+    <!-- Banner de presentación -->
     <section id="banner" class="container-fluid p-0">
         <div class="banner-img" style="position:relative; background:url('images/foodRestaurant3.jpg') center/cover no-repeat; height:500px;">
             <div class="banner-text" style="position:absolute; top:50%; left: 50%; transform: translate(-50%, -50%); text-align:center; color:#fff">
@@ -55,6 +59,7 @@
             </div>
         </div>
     </section>
+    <!-- Información de presentación -->
     <section id="id" class="container mt-4 text-center">
 
         <div class="jumbotron bg-dark text-white">
@@ -65,11 +70,11 @@
         </div>
 
     </section>
-
+    <!-- Sección de Chefs -->
     <section id="chefs" class="container mt-4 text-center">
         <h2>Nuestros Chefs!</h2>
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-4 d-flex">
                 <div class="card">
                     <img src="images/colaboradores/Chef-Carlo.jpg" alt="Carlo" class="card-img-top">
                     <div class="card-body">
@@ -83,7 +88,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-4 d-flex">
                 <div class="card">
 
                     <img src="images/colaboradores/Chef-Chan.jpg" alt="Chan" class="card-img-top">
@@ -98,7 +103,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-4 d-flex">
                 <div class="card">
                     <img src="images/colaboradores/Chef-Nicola.jpg" alt="Nicola" class="card-img-top">
                     <div class="card-body">
@@ -115,7 +120,7 @@
         </div>
 
     </section>
-
+    <!-- Sección de Testimonios -->
     <section id="testimonios" class="bg-light py-5">
         <div class="container">
             <h2 class="text-center mb-4">Testimonios</h2>
@@ -156,13 +161,96 @@
             </div>
         </div>
     </section>
+    <!-- Sección Menú del día -->
+    <section id="menu" class="container mt-4">
+        <h2 class="text-center">Menú (Nuestra recomendación)</h2>
+        <br>
+        <div class="row row-cols-1 row-cols-md-4 g-4">
+            <div class="col d-flex">
+                <div class="card h-100">
+                    <img src="images/menu/plato1.jpg" alt="Pasta" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Pasta en salsa de tomate con pollo y tomates</h5>
+                        <p class="card-text small"><strong>Ingredientes: </strong>Pasta, pollo, tomates, pasta de tomates</p>
+                        <p class="card-text"> <strong>Precio: </strong>$25.000 </p>
+                    </div>
+                </div>
 
+            </div>
+            <div class="col d-flex">
+                <div class="card h-100">
+                    <img src="images/menu/plato2.jpg" alt="Cerdo" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Cerdo con arroz y patatas</h5>
+                        <p class="card-text small"><strong>Ingredientes: </strong>Cerdo, papas, tomates</p>
+                        <p class="card-text"> <strong>Precio: </strong>$30.000 </p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col d-flex">
+                <div class="card h-100">
+                    <img src="images/menu/plato3.jpg" alt="Carne" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Carne frita con papas fritas y salsa de tomate</h5>
+                        <p class="card-text small"><strong>Ingredientes: </strong>Carne frita, papas fritas, salsa de tomates</p>
+                        <p class="card-text"> <strong>Precio: </strong>$35.000 </p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col d-flex">
+                <div class="card h-100">
+                    <img src="images/menu/plato3.jpg" alt="Carne" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Carne frita con papas fritas y salsa de tomate</h5>
+                        <p class="card-text small"><strong>Ingredientes: </strong>Carne frita, papas fritas, salsa de tomates</p>
+                        <p class="card-text"> <strong>Precio: </strong>$35.000 </p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+    <!-- Sección Contacto -->
+    <section id="contacto" class="container mt-4">
+        <h2>Contacto</h2>
+        <p>Estamos aquí para servirle.</p>
+        <form action="?" method="post">
+            <label for="name">Nombre:</label><br>
+            <input type="text" class="form-control" name="nombre" placeholder="Escribe tu nombre..." required><br>
+            <label for="email">Correo electrónico:</label><br>
+            <input type="email" name="correo" class="form-control" placeholder="Escribe tu correo electrónico..." required><br>
+            <label for="message">Mensaje:</label><br>
+            <textarea class="form-control" name="mensaje" rows="6" cols="50"></textarea><br>
+            <input type="submit" class="btn btn-primary" value="Enviar mensaje">
+        </form>
+
+    </section>
     <br />
+    <!--Sección Horarios-->
+    <div class="text-center bg-light p-4" id="horario">
+        <h3 class="mb-4">Horario de atención</h3>
+        <div>
+            <p><strong>Lunes a viernes</strong></p>
+            <p><strong>11:00 a.m. - 10:00 p.m.</strong></p>
+        </div>
+        <div>
+            <p><strong>Sábados</strong></p>
+            <p><strong>12:00 a.m. - 5:00 p.m.</strong></p>
+        </div>
+        <div>
+            <p><strong>Domingos</strong></p>
+            <p><strong> 7:00 a.m. - 2:00 p.m.</strong></p>
+        </div>
+    </div>
+
     <header>
         <!-- place navbar here -->
     </header>
     <main></main>
-    <footer class="bg-dark text-light text-center">
+    <footer class="bg-dark text-light text-center py-3">
         <!-- place footer here -->
         <p>&copy; 2024 Bonka's, todos los derechos reservados.</p>
     </footer>
