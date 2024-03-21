aa0064276fda730f8a5f60e0c7fbe63da5ab5f52
jose barros <jolubaqui@gmail.com>
2024-03-20 21:54:51 -0500

    Listando personal

---
+++ admin/seccion/personal/crear.php
@@ -0,0 +1,96 @@
+<?php
+include("../../bd.php");
+
+if ($_POST) {
+    $nombre = (isset($_POST['nombre'])) ? $_POST['nombre'] : "";
+    $foto = (isset($_POST['foto'])) ? $_POST['foto'] : "";
+    $descripcion = (isset($_POST['descripcion'])) ? $_POST['descripcion'] : "";
+    $linkfacebook = (isset($_POST['linkfacebook'])) ? $_POST['linkfacebook'] : "";
+    $linkinstagram = (isset($_POST['linkinstagram'])) ? $_POST['linkinstagram'] : "";
+    $linklinkedin = (isset($_POST['linklinkedin'])) ? $_POST['linklinkedin'] : "";
+
+    if (empty($nombre) || empty($foto) || empty($descripcion) || empty($linkfacebook) || empty($linkinstagram) || empty($linklinkedin)) {
+        echo "Por favor, completa todos los campos del formulario.";
+    } else {
+        $sentencia = $conexion->prepare('INSERT INTO tbl_personal 
+        (ID, nombre, descripcion, linkfacebook, linkinstagram, linklinkedin, foto) 
+        VALUES(NULL, :nombre ,:descripcion, :linkfacebook, :linkinstagram, :linklinkedin, :foto)');
+
+
+        // Se asignan los valores a las variables en la sentencia preparada
+        $sentencia->bindParam(':nombre', $nombre);
+        $sentencia->bindParam(':foto', $foto);
+        $sentencia->bindParam(':descripcion', $descripcion);
+        $sentencia->bindParam(':linkfacebook', $linkfacebook);
+        $sentencia->bindParam(':linkinstagram', $linkinstagram);
+        $sentencia->bindParam(':linklinkedin', $linklinkedin);
+
+        if ($sentencia->execute()) {
+            header("location:index.php");
+            exit(); // Asegura que el script se detenga después de la redirección
+        } else {
+            echo "Error al insertar el banner.";
+        }
+    }
+} else {
+    echo "No se recibió ninguna información por parte del usuario.";
+}
+
+include("../../templates/header.php");
+?>
+
+<br>
+<div class="card">
+    <div class="card-header">Banners</div>
+    <div class="card-body">
+
+        <form action="" method="post">
+
+
+            <div class="mb-3">
+                <label for="" class="form-label">Nombre:</label>
+                <input type="text" class="form-control" name="nombre" id="nombre" aria-describedby="helpId" placeholder="Coloque el nombre del empleado" />
+
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Foto:</label>
+                <input type="text" class="form-control" name="foto" id="foto" aria-describedby="helpId" placeholder="Coloque una foto del empleado" />
+
+            </div>
+            <div class="mb-3">
+                <label for="descripcion" class="form-label">Descripción:</label>
+                <input type="text" class="form-control" name="descripcion" id="descripcion" aria-describedby="helpId" placeholder="Ingrese la descripción" />
+
+            </div>
+
+            <div class="mb-3">
+                <label for="linkfacebook" class="form-label">linkfacebook:</label>
+                <input type="text" class="form-control" name="linkfacebook" id="linkfacebook" aria-describedby="helpId" placeholder="Ingrese el enlace" />
+
+            </div>
+
+            <div class="mb-3">
+                <label for="linkinstagram" class="form-label">Linkinstagram:</label>
+                <input type="text" class="form-control" name="linkinstagram" id="linkinstagram" aria-describedby="helpId" placeholder="Ingrese el enlace" />
+
+            </div>
+            <div class="mb-3">
+                <label for="linklinkedin" class="form-label">Linklinkedin:</label>
+                <input type="text" class="form-control" name="linklinkedin" id="linklinkedin" aria-describedby="helpId" placeholder="Ingrese el enlace" />
+
+            </div>
+
+            <button type="submit" class="btn btn-success">Ingresar persona</button>
+            <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
+
+
+        </form>
+
+    </div>
+    <div class="card-footer text-muted"></div>
+</div>
+
+
+<?php
+include("../../templates/footer.php")
+?>
---
+++ admin/seccion/personal/index.php
@@ -0,0 +1,67 @@
+<?php
+include("../../bd.php");
+
+if (isset($_GET["txtID"])) {
+
+    $txtID = (isset($_GET["txtID"])) ? $_GET['txtID'] : "";
+    $sentencia = $conexion->prepare("DELETE FROM tbl_personal WHERE ID = :id");
+    $sentencia->bindParam(":id", $txtID);
+    $sentencia->execute();
+    header("Location:index.php");
+}
+
+$sentencia = $conexion->prepare("SELECT * FROM tbl_personal");
+$sentencia->execute();
+$lista_personal = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+
+
+include("../../templates/header.php");
+?>
+<br>
+<div class="card">
+    <div class="card-header">
+        <a name="" id="" class="btn btn-primary" href="crear.php" role="button">Crear registro</a>
+        <h1>Colaboradores</h1>
+    </div>
+    <div class="card-body">
+
+        <div class="table-responsive-sm">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Foto</th>
+                        <th scope="col">Descripción</th>
+                        <th scope="col">Redes Sociales</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    foreach ($lista_personal as $key => $value) { ?>
+                        <tr class="">
+                            <td scope="row"><?php echo ($value['ID']); ?></td>
+                            <td><?php echo ($value['nombre']); ?></td>
+                            <td><?php echo ($value['foto']); ?></td>
+                            <td><?php echo ($value['descripcion']); ?></td>
+                            <td><?php echo ($value['linkfacebook']); ?><br>
+                                <?php echo ($value['linkinstagram']); ?><br>
+                                <?php echo ($value['linklinkedin']); ?></td>
+                            <td><a name="" id="" class="btn btn-info" href="editar.php?txtID=<?php echo $value["ID"]; ?>" role="button">Editar</a>
+                                <a name="" id="" class="btn btn-danger" href="index.php?txtID=<?php echo $value["ID"]; ?>" role="button">Borrar</a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+
+                </tbody>
+            </table>
+        </div>
+
+
+    </div>
+    <div class="card-footer text-muted"></div>
+</div>
+
+
+<?php include("../../templates/footer.php") ?>
