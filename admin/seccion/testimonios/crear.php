6e707cf9cf32dadcaa69995fe7aad2a9acc31f1a
jose barros <jolubaqui@gmail.com>
2024-03-23 11:25:59 -0500

    crear testimonios

--- admin/seccion/testimonios/crear.php
+++ admin/seccion/testimonios/crear.php
@@ -1,8 +1,60 @@
 <?php
 include("../../bd.php");
+
+if ($_POST) {
+    print_r($_POST);
+
+    $opinion = (isset($_POST['opinion'])) ? $_POST['opinion'] : '';
+    $nombre = (isset($_SESSION['nombre'])) ? $_POST['nombre'] : '';
+
+    $sentencia = $conexion->prepare('INSERT INTO 
+    tbl_testimonios (NULL,opinion, nombre) VALUES    
+( :opinion ,:nombre)');
+
+    $nombre = $_POST['nombre'];
+    $opinion = $_POST['opinion'];
+
+    //Se pasan los valores a la sentencia preparada para evitar inyecciones SQL
+    $sentencia->bindParam(":opinion", $opinion);
+    $sentencia->bindParam(":nombre", $nombre);
+
+    $resultado = $sentencia->execute(); //Ejecución de la consulta
+
+    /*Si todo ha ido bien (ninguna fila afectada), se devuelve un mensaje y redirecciona a la página principal*/
+    if ($resultado == true) {
+        echo 'Testimonio enviado correctamente';
+        header('Location: ../index.php');
+    } else { /*En caso de error, muestra un mensaje de error*/
+        die('Error al enviar el testimonio');
+    }
+} else {
+    die("No hay datos en este formulario");
+}
+
 include("../../templates/header.php");
 ?>
 
+<br>
+<div class="card">
+    <div class="card-header">TESTIMONIOS</div>
+    <div class="card-body">
+
+        <form action="" method="post">
+
+            <div class="mb-3">
+                <label for="opinion" class="form-label">Opinión</label>
+                <input type="text" class="form-control" name="opinion" id="opinion" aria-describedby="helpId" placeholder="" />
+            </div>
+            <div class="mb-3">
+                <label for="nombre" class="form-label">Nombre</label>
+                <input type="text" class="form-control" name="nombre" id="nombre" aria-describedby="helpId" placeholder="" />
+            </div>
+            <button type="submit" class="btn btn-success">Agregar opinión</button>
+            <a href="index.php" role="button" class="btn btn-primary">Cancelar</a>
+        </form>
+    </div>
+    <div class="card-footer text-muted"></div>
+</div>
 
 
 <?php
--- admin/seccion/testimonios/index.php
+++ admin/seccion/testimonios/index.php
@@ -1,9 +1,52 @@
 <?php
 include("../../bd.php");
+
+$sentencia = $conexion->prepare("SELECT * FROM tbl_testimonios ");
+$sentencia->execute();
+$lista_testimonios = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+
+
+
 include("../../templates/header.php");
 ?>
+<br>
+<div class="card">
+    <div class="card-header">
+        <a href="crear.php" role="button" class="btn btn-primary">Agregar registro</a>
+        <h1>Testimonios</h1>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive-sm">
+            <table class="table ">
+                <thead>
 
-<img src="../../../images/EnConstruccion.jpg" alt="">
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Opinión</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Acción</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($lista_testimonios as $$key => $value) { ?>
+                        <tr class="">
+                            <td scope="row"><?php echo (value['ID']) ?></td>
+                            <td><?php echo (value['opinion']) ?></td>
+                            <td><?php echo (value['nombre']) ?></td>
+                            <td><a name="" id="" class="btn btn-info" href="editar.php?txtID=<?php echo $value["ID"]; ?>" role="button">Editar</a>
+                                <a name="" id="" class="btn btn-danger" href="index.php?txtID=<?php echo $value["ID"]; ?>" role="button">Borrar</a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+    <div class="card-footer text-muted"></div>
+</div>
+
 
 <?php
 include("../../templates/footer.php");
